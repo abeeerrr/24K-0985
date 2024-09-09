0092872c212539e071b9ec64bad335f7e5abbec4
abeeerrr <lifeofapii@gmail.com>
2024-09-09 23:46:13 +0500

    Add files via upload

---
+++ q3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+float salary, taxRate, tax, salary;
+printf("Take your salary\n");
+scanf("%f",&salary);
+printf("Enter the tax rate\n");
+scanf("%f",&taxRate);
+tax=(salary*taxRate)/100);
+salary=salary-tax;
+printf("tax payable is: %f\n", tax);
+printf("salary after tax is: %f\n", salary);
+return 0;
+}
