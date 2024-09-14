76fb13493ebea4c24172372abfe5f54557c209fc
abeeerrr <lifeofapii@gmail.com>
2024-09-14 23:34:09 +0500

    Update q4.c

--- Lab 3/q4.c
+++ Lab 3/q4.c
@@ -3,8 +3,8 @@ int main() {
 int distance, fuelCostComing, fuelCostGoing;
 float fuel1, fuel2, totalFuelCost, totalFuelUsed, fuelcost1, fuelcost2;
 distance = 1207;
-fuelCostComing=118;
-fuelCostGoing=123;
+fuelCostComing = 118;
+fuelCostGoing = 123;
 printf("How much fuel was consumed per km while travelling to B\n");
 scanf ("%f", &fuel1);
 printf("How much fuel was consumed per km while travelling back to A\n");
