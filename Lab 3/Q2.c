1f8c8140661c658ae5530e3f4d762dad9b9142af
abeeerrr <lifeofapii@gmail.com>
2024-09-14 22:20:34 +0500

    Add files via upload

---
+++ Lab 3/Q2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+int x,y,z;
+ printf("take any two numbers\n");
+ scanf("%d%d",&x,&y);
+   z = x;
+   x = y;
+   y = z;
+ printf("swapped value of x\n");
+ printf("swapped value of y\n");
+		
+return 0;
+}
+			
